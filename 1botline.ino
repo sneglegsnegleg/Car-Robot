16bd2ac0480d287b6936a971abdbe1b208ce94e2
sneglegsnegleg <sebourque@mail.roanoke.edu>
2025-10-27 12:02:30 -0400

    mateo code

---
+++ 1botline.ino
@@ -0,0 +1,108 @@
+const int A_1B = 5;
+const int A_1A = 6;
+const int B_1B = 9;
+const int B_1A = 10;
+
+const int lineTrackM = 2;
+const int lineTrackL = A0;
+const int lineTrackR = A1;
+
+void setup() {
+  Serial.begin(9600);
+
+  //motor
+  pinMode(A_1B, OUTPUT);
+  pinMode(A_1A, OUTPUT);
+  pinMode(B_1B, OUTPUT);
+  pinMode(B_1A, OUTPUT);
+  //line track
+  pinMode(lineTrackM, INPUT);
+  pinMode(lineTrackL, INPUT);
+  pinMode(lineTrackR, INPUT);
+}
+int d = 0;
+
+void loop() {
+  int speed = 150;
+int lineColorL  = analogRead(A0);
+int lineColorR = analogRead(A1);
+
+  int lineColorM = digitalRead(lineTrackM); // 0:white  1:black
+  Serial.println(lineColorM); //print on the serial monitor
+  //Serial.println(lineColorR); //print on the serial monitor
+  Serial.println(lineColorR); //print on the serial monitor
+delay(0);
+  if (lineColorM == 1 && lineColorR < 500 && lineColorL < 500) { //only middle sensor reads black
+   moveForward(speed);
+  } 
+ else if (lineColorR > 500 && lineColorL < 500 && lineColorM == 0) { //only Right sensor reads black
+     moveRight(speed, 0);
+  } else if (lineColorL > 500 && lineColorR < 500 && lineColorM == 0) { //only Left sensor reads black
+     moveLeft(speed, 0);
+  }
+ else
+  if (lineColorM == 1 && lineColorR > 500 && lineColorL < 500) { //right and middle sensor read black
+    moveRight(speed, 0);
+  } else if (lineColorM == 1 && lineColorR < 500 && lineColorL > 500) { //left and middle sensor read black
+    moveLeft(speed, 0);
+  } else if (lineColorM == 1 && lineColorR > 500 && lineColorL > 500) { //all read black
+   moveForward(speed);
+   }else {
+    //moveBackward(speed); 
+  }
+
+}
+void stop(int speed) {
+  analogWrite(A_1B, 0);
+  analogWrite(A_1A, 0);
+  analogWrite(B_1B, 0);
+  analogWrite(B_1A, 0);
+}
+void moveForward(int speed) {
+  analogWrite(A_1B, 0);
+  analogWrite(A_1A, speed);
+  analogWrite(B_1B, speed);
+  analogWrite(B_1A, 0);
+}
+void moveBackward(int speed) {
+  analogWrite(A_1B, speed);
+  analogWrite(A_1A, 0);
+  analogWrite(B_1B, 0);
+  analogWrite(B_1A, speed);
+}
+
+
+void moveLeft(int speed1, int speed2) {
+  analogWrite(A_1B, 0);
+  analogWrite(A_1A, speed1);
+  analogWrite(B_1B, 0);
+  analogWrite(B_1A, speed2);
+}
+
+void moveRight(int speed1, int speed2) {
+  analogWrite(A_1B, speed2);
+  analogWrite(A_1A, 0);
+  analogWrite(B_1B, speed1);
+  analogWrite(B_1A, 0);
+}
+bool checkLeft(int lineColorL, int speed) {
+  //moveLeft(speed); removed cuz Left sensor is checking
+    lineColorL = analogRead(lineTrackL); // 0:white  1:black
+  Serial.println("Left Sensor: ");
+  Serial.println(lineColorL);
+  if (lineColorL < 500) {
+    return false;
+  } else {
+  return true;
+}
+}
+bool checkRight(int lineColorR, int speed) {
+  // moveRight(speed); removed cuz Right sensor is checking
+  lineColorR = analogRead(lineTrackR); // 0:white  1:black
+  Serial.println(lineColorR);
+  if (lineColorR < 500) {
+    return false;
+  }else {
+  return true;
+}
+}
